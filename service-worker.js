1504c09ebf01872a38b9f4489ec51aada7b0606e
roilevi123 <roi9@post.bgu.ac.il>
2025-08-04 17:20:12 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const cacheName = 'trip-app-cache-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/service-worker.js',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
